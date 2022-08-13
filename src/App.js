6f3db5445ce268460ff4b0e73ba322e9e6f5587d
Vinícius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2022-08-13 14:27:53 -0300

    Imported First Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// Components
+import FirstComponent from './components/FirstComponent';
+
+// Styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
-      <h2>Emmet ok</h2>
+      <FirstComponent />
     </div>
   );
 }
