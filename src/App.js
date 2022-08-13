d2c19a096a1b69a5fcd0c706709ddf49750706b5
Vinícius Schnoor <72426583+viniciusschnoor@users.noreply.github.com>
2022-08-13 14:21:46 -0300

    Created First Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
+      <h2>Emmet ok</h2>
     </div>
   );
 }
